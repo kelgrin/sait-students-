91a769da9417d5cceac1bf58b434fafa73c61e56
kelgrin <kelgrin@mail.ru>
2024-12-20 23:50:37 +0600

    2

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,15 +1,8 @@
-
 import prisma from "@/lib/prisma"
 import styles from "./page.module.css";
 import Image from "next/image";
 
 // Пример данных о товарах
-const products = [
-  { id: "1", title: "Tango", price: "90 Gold", imgSrc: "/plugin1.png", description: "Берите всегда на лайн." },
-  { id: "2", title: "Observer ward", price: "Free!", imgSrc: "/plugin2.png", description: "Сапортам всегда покупать, они бесплатные!." },
-  { id: "3", title: "Smoke of Deceit", price: "50 Gold ", imgSrc: "/plugin3.png", description: "Многие о нем забывают, но вы не совершайте чужие ошибки." },
-  { id: "4", title: "Dust of Appearance", price: "80 Gold", imgSrc: "/plugin4.png", description: "Невидимость контрится этим, не глупите и берите." },
-];
 
 export default async function ProductPage({ params }) {
   const { id } = await params
